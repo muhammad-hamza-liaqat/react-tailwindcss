c4b373deea4b078b1b6fb84b2db9243d46684fd3
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-16 12:00:17 +0500

    Inside banner

---
+++ src/components/Header/InsideBanner.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function InsideBanner() {
+    return (
+        <>
+            <div className='bg-black mt-20 p-10 text-white'>
+                <p className='text-center font-bold text-6xl'>
+                    Back a campaign, to launch yours automatically - for free
+                </p>
+
+                <p className=' text-center mt-10 font-medium text-3xl'>
+                    We waive the hosting fee for your new campaign when you are back
+                </p>
+            </div>
+
+        </>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Banner from '../components/Header/Banner'
 import MenuBar from '../components/Header/MenuBar'
 import Introduction from '../components/Header/Introduction'
 import Campaign from '../components/Header/Campaign'
+import InsideBanner from '../components/Header/InsideBanner'
 
 export default function Home() {
     return (
@@ -11,6 +12,7 @@ export default function Home() {
             <MenuBar />
             <Introduction />
             <Campaign />
+            <InsideBanner />
         </>
     )
 }
