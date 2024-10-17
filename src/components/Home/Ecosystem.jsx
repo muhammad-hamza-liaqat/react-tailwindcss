723e24b37cb7e82a449d79bb0170a250ba3caaa2
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-17 12:04:41 +0500

    hello

--- src/components/Home/Ecosystem.jsx
+++ src/components/Home/Ecosystem.jsx
@@ -6,6 +6,10 @@ import powered1 from "../../assets/images/powered1.png"
 import powered2 from "../../assets/images/powered2.png"
 import powered3 from "../../assets/images/powered3.png"
 import powered4 from "../../assets/images/powered4.png"
+import c1 from "../../assets/images/c1.png"
+import c2 from "../../assets/images/c2.png"
+import c3 from "../../assets/images/c3.png"
+import c4 from "../../assets/images/c4.png"
 
 export default function Ecosystem() {
     return (
@@ -44,9 +48,25 @@ export default function Ecosystem() {
                     </div>
                 </div>
 
+                {/* content */}
 
                 <div className="c4">
                     <div className="head flex justify-center mt-1">
+                        <p className='text-2xl font-bold '>Content & Benefits</p>
+                    </div>
+
+                    <div className='image3 flex p-2 mt-10 justify-center'>
+                        <img src={c1} alt="" className='w-30 h-24 object-contain mr-5' />
+                        <img src={c2} alt="" className='w-24 h-24 object-contain mr-5' />
+                        <img src={c3} alt="" className='w-24 h-24  object-contain mr-5 bg-slate-800 rounded-3xl' />
+                        <img src={c4} alt="" className='w-24 h-24 object-contain bg-slate-800 rounded-3xl' />
+                    </div>
+
+                </div>
+
+
+                <div className="c4">
+                    <div className="head flex justify-center mt-10">
                         <p className='text-2xl font-bold bg-red-700 text-white rounded-2xl p-2 '>Tissue Box</p>
                         <p className='text-2xl font-bold p-2'>is powdered by</p>
                     </div>
