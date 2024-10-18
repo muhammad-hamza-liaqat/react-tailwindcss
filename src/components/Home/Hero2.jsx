b9c56ac8d717e5952ee7abd1f91709df8e3fc1b3
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-18 12:32:50 +0500

    responsive

--- src/components/Home/Hero2.jsx
+++ src/components/Home/Hero2.jsx
@@ -5,7 +5,7 @@ export default function Hero2() {
         <>
             <div className="Parent mt-8">
                 <div className='p2'>
-                    <div className='flex flex-col md:justify-around'>
+                    <div className='flex flex-col md:flex md:flex-row md:justify-around'>
                         <div className='flex flex-col items-center mt-5 border rounded-2xl py-5 px-2 text-2xl font-bold'> Tissue Box Sold  <div className='text-center mt-10'> 67 </div></div>
                         <div className='mt-5 border  rounded-2xl py-5 px-2 bg-black'> <div> <p className='text-white'> video added to be here here herer</p></div></div>
                         <div className='mt-5 border  rounded-2xl py-5 px-2 bg-slate-500'>
@@ -19,11 +19,11 @@ export default function Hero2() {
                                 <p className='mr-9'>Quantity</p>
                                 <textarea
                                     id="amount"
-                                    className='rounded-sm p-1 w-20 h-10'
+                                    className='rounded-lg p-1 w-20 h-10'
                                     defaultValue="amount"
                                 ></textarea>
                             </div>
-                            <div>
+                            <div className='mt-3 '>
                                 <div className='flex items-center'>
                                     <p className='mr-14'>USTD</p>
                                     <button className='bg-red-600 px-1 py-2 rounded-md text-white'>Connect</button>
