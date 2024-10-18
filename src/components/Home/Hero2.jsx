7707dd956fc9dfd415e71225a823a0c44da9a9d5
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-18 12:20:38 +0500

    hero2

--- src/components/Home/Hero2.jsx
+++ src/components/Home/Hero2.jsx
@@ -6,16 +6,17 @@ export default function Hero2() {
             <div className="Parent mt-8">
                 <div className='p2'>
                     <div className='flex justify-around'>
-                        <div className='flex flex-col items-center'> Tissue Box Sold  <div> 67 </div></div>
-                        <div> Video to be added here</div>
-                        <div> Checkout
+                        <div className='flex flex-col items-center mt-5 border border-black rounded-2xl py-5 px-2 text-2xl font-bold'> Tissue Box Sold  <div className='text-center mt-10'> 67 </div></div>
+                        <div className='mt-5 border border-black rounded-2xl py-5 px-2'> Video to be added here</div>
+                        <div className='mt-5 border border-black rounded-2xl py-5 px-2'>
+                            <p className='text-center font-bold mb-2'> Checkout</p>
                             <div className='flex items-center'>
                                 <p>Current Price</p>
                                 <p> 200 USTD </p>
                             </div>
 
-                            <div className='flex mt-4'>
-                                <p>Quantity</p>
+                            <div className='flex mt-4 items-center'>
+                                <p className='mr-9'>Quantity</p>
                                 <textarea
                                     id="amount"
                                     className='rounded-sm p-1 w-20 h-10'
@@ -23,14 +24,15 @@ export default function Hero2() {
                                 ></textarea>
                             </div>
                             <div>
-                                <div className='flex'>
-                                    <p>USTD</p>
+                                <div className='flex items-center'>
+                                    <p className='mr-14'>USTD</p>
                                     <button className='bg-red-600 px-1 py-2 rounded-md text-white'>Connect</button>
                                 </div>
-                                <div>
-                                    <p>
-                                    Price Increase per 200 sold will be +20 USDT (Starting from #10,001)
+                                <div className=' mt-5 flex flex-col'>
+                                    <p className='text-xs'>
+                                        Price Increase per 200 sold will be +20 USDT (Starting
                                     </p>
+                                    <p className='text-xs'>from #10,001)</p>
                                 </div>
                             </div>
 
