5a94ac0993b14f844a0375fb67136f4e951b75b6
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-23 09:56:32 +0500

    powdered

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Nav2 from '../components/NavBar/Nav2'
 import Hero2 from '../components/Home/Hero2'
 import Eco2 from '../components/Home/Eco2'
 import Foot from '../components/Footer/Foot'
+import Powered from '../components/Home/Powered'
 
 export default function Home() {
     return (
@@ -10,6 +11,7 @@ export default function Home() {
         <Nav2/>
         <Hero2/>
         <Eco2/>
+        <Powered/>
         <Foot/>
         </>
     )
