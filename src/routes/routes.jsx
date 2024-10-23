1b4bea964182a5ea3a0737904bd528397562ebc8
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-23 16:55:39 +0500

    code clean

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,10 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom"
-import Home from "../pages/Home"
 import Profile from "../pages/Profile"
 
 const MyRoute = () => {
     return (
         <Routes>
-            {/* <Route path="/" element={<Home />} /> */}
             <Route path="/" element={<Profile />} />
         </Routes>
     )
